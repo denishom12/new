4cf6d4eb266fd8c5057786c699e7aa9a6bd1ba27
222 <denishom12@gmail.com>
2021-03-01 21:37:42 +0300

    ex

--- mysite-vue/src/router/index.js
+++ mysite-vue/src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Default from "../layouts/Default";
-import Auth from "../layouts/Auth";
-import Login from "../views/Login";
-import Registration from "../views/Registration";
+import Main from "../views/Main";
 
 Vue.use(VueRouter)
 
@@ -13,42 +9,9 @@ const routes = [
     path: '/',
     name: 'Default',
     redirect: '/home',
-    component: Default,
-    children: [
-      {
-        path: 'home',
-        name: 'home',
-        component: Home
-      }
-    ]
-  },
-  {
-    path: '/auth',
-    name: 'Auth',
-    component: Auth,
-    children: [
-      {
-        path: 'login',
-        name: 'login',
-        component: Login
-      },
-      {
-        path: 'register',
-        name: 'register',
-        component: Registration
-      }
-    ]
-  },
-  {
-    path: '/login',
-    redirect: '/auth/login',
-  },
-  {
-    path: '/register',
-    redirect: '/auth/register'
+    component: Main
   }
 ]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
